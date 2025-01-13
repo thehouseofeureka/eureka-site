64b9261a126fb8c4401d702d7e3f95f2d56e7974
Preston Tang <prestonleetang0123@gmail.com>
2025-01-13 16:57:23 -0500

    Added prototype of flower and footer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,32 @@
 // src/app/home/page.tsx
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Image from 'next/image'
+import styles from './page.module.css'
 
-export default function Hero() {
-  return <Navbar />
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.imageContainer}>
+        <div className={styles.flowerWrapper}>
+          <Image
+            src="/flower.svg"
+            alt="Flower"
+            width={200}
+            height={200}
+            className={styles.flowerClockwise}
+          />
+          <Image
+            src="/flower.svg"
+            alt="Flower"
+            width={200}
+            height={200}
+            className={styles.flowerCounterClockwise}
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
